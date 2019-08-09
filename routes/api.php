c448b66f4879c3616b5f75a6183f4495769c8ffc
Aquino Tegar <aquinotgr@pm.me>
2019-08-09 18:18:14 +0700

    combine the backend api routes

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,6 @@
 <?php
 
 Route::group(['as' => 'volunteers.'], function () {
-    Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'auth:admin'], function () {
-        Route::get('/', 'VolunteerApiController@index')->name('list');
-        Route::apiResource('volunteers', 'VolunteerApiController');
-        Route::get('export-volunteers/print', 'VolunteerApiController@print')->name('print');
-        Route::get('export-volunteers/print-profile/{volunteer}', 'VolunteerApiController@printProfile')->name('print-profile');
-    });
     // ============================================================= Back-end
     Route::prefix(config('admin.prefix'))->middleware('auth:admin')->name('admin.')->group(function() {
         Route::group(['prefix' => 'settings'], function () {
@@ -16,18 +10,22 @@ Route::group(['as' => 'volunteers.'], function () {
             Route::apiResource('/village', 'VillageApiController');
         });
         
+        Route::get('/', 'VolunteerApiController@index')->name('list');
+        Route::get('export-volunteers/print', 'VolunteerApiController@print')->name('print');
+        Route::get('export-volunteers/print-profile/{volunteer}', 'VolunteerApiController@printProfile')->name('print-profile');
+        
         Route::apiResource('volunteers', 'VolunteerApiController');
     });
-});
 
-// ============================================================= Front-end
-Route::prefix('app')->group(function () {
-    Route::prefix('area')->group(function() {
-        Route::get('provinces', 'ProvinceApiController@index')->name('province.list');
-        Route::get('cities', 'CityApiController@index')->name('city.list');
-        Route::get('subdistricts', 'SubdistrictApiController@index')->name('subdistrict.list');
-        Route::get('urbanvillages', 'VillageApiController@index')->name('village.list');
+    // ============================================================= Front-end
+    Route::prefix('app')->group(function () {
+        Route::prefix('area')->group(function() {
+            Route::get('provinces', 'ProvinceApiController@index')->name('province.list');
+            Route::get('cities', 'CityApiController@index')->name('city.list');
+            Route::get('subdistricts', 'SubdistrictApiController@index')->name('subdistrict.list');
+            Route::get('urbanvillages', 'VillageApiController@index')->name('village.list');
+        });
+        
+        Route::post('volunteer/signup', 'VolunteerApiController@store');
     });
-    
-    Route::post('volunteer/signup', 'VolunteerApiController@store');
 });
