bb684cabf0bc2f5f4c990bcae1127fe901ca84cb
Aquino Tegar <aquinotgr@pm.me>
2019-08-09 17:15:23 +0700

    fix missing end of line

--- routes/api.php
+++ routes/api.php
@@ -7,12 +7,13 @@ Route::group(['as' => 'volunteers.'], function () {
         Route::get('export-volunteers/print', 'VolunteerApiController@print')->name('print');
         Route::get('export-volunteers/print-profile/{volunteer}', 'VolunteerApiController@printProfile')->name('print-profile');
     });
-// ============================================================= Back-end
-Route::group(['prefix' => 'admin/settings', 'as' => 'admin.', 'middleware' => 'auth:admin'], function () {
-    Route::apiResource('/province', 'ProvinceApiController');
-    Route::apiResource('/city', 'CityApiController');
-    Route::apiResource('/subdistrict', 'SubdistrictApiController');
-    Route::apiResource('/village', 'UrbanVillageApiController');
+    // ============================================================= Back-end
+    Route::group(['prefix' => 'admin/settings', 'as' => 'admin.', 'middleware' => 'auth:admin'], function () {
+        Route::apiResource('/province', 'ProvinceApiController');
+        Route::apiResource('/city', 'CityApiController');
+        Route::apiResource('/subdistrict', 'SubdistrictApiController');
+        Route::apiResource('/village', 'UrbanVillageApiController');
+    });
 });
 
 // ============================================================= Front-end
