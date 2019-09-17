3dfa2a0fb82902750e10d65f67a4d45e90e024d9
Aquino Tegar <aquinotgr@pm.me>
2019-09-17 20:57:16 +0700

    change admin id to random active admin

--- database/factories/EventActivitiesFactory.php
+++ database/factories/EventActivitiesFactory.php
@@ -6,6 +6,7 @@ use BajakLautMalaka\PmiRelawan\EventParticipant;
 use BajakLautMalaka\PmiRelawan\EventReport;
 use BajakLautMalaka\PmiRelawan\EventActivity;
 use Faker\Generator as Faker;
+use BajakLautMalaka\PmiAdmin;
 
 $factory->define(EventActivity::class, function (Faker $faker, $params) {
     $volunteerId = EventReport::find($params['event_report_id'])->participants->random()->volunteer->id;
@@ -14,6 +15,6 @@ $factory->define(EventActivity::class, function (Faker $faker, $params) {
       'event_report_id' => $params['event_report_id'],
       'volunteer_id' => $isAdmin ? null:$volunteerId,
       'comment' => $faker->sentence,
-      'admin_id' => $isAdmin ? 1:null
+      'admin_id' => $isAdmin ? Admin::active()->get()->random()->id:null
     ];
 });
