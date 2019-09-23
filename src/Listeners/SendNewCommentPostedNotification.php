a9a4d070d483a35e83a1ffb3086d95951410e53b
Paulus Otto Harman <lonewolfffffff@gmail.com>
2019-09-23 20:16:14 +0700

    new comment/EventActivity : replace push notification message "sesuatu" with "pesan"

--- src/Listeners/SendNewCommentPostedNotification.php
+++ src/Listeners/SendNewCommentPostedNotification.php
@@ -29,7 +29,7 @@ class SendNewCommentPostedNotification
     {
         $name = $commentPosted->comment->volunteer_id?$commentPosted->comment->volunteer->name:$commentPosted->comment->admin->name;
         $this->pushNotificationClient->sendNotificationToSegment(
-            $name.' mengirimkan sesuatu di '.$commentPosted->comment->event->title,
+            $name.' mengirimkan pesan di '.$commentPosted->comment->event->title,
             'Volunteers',
             null,
             ['rsvpId'=>$commentPosted->comment->event_report_id]
